8bdd84c04389f646fff9a200545f58d4e5899225
Ayush Sahu <ayushsahu29042005@gmail.com>
2026-07-22 12:27:47 +0530

    Add files via upload

---
+++ UC 1/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+
+        System.out.print("Enter Address: ");
+        String address = sc.nextLine();
+
+        System.out.print("Enter City: ");
+        String city = sc.nextLine();
+
+        System.out.print("Enter State: ");
+        String state = sc.nextLine();
+
+        System.out.print("Enter Zip: ");
+        String zip = sc.nextLine();
+
+        System.out.print("Enter Phone Number: ");
+        String phone = sc.nextLine();
+
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
+
+        Contact contact = new Contact(firstName, lastName, address,
+                                      city, state, zip, phone, email);
+
+        System.out.println("\n----- Contact Details -----");
+        System.out.println(contact);
+
+        sc.close();
+    }
+}

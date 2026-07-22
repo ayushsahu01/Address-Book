0cbcf0a561e8aaa387471d9e9443a2be81b069de
Ayush Sahu <ayushsahu29042005@gmail.com>
2026-07-22 12:25:21 +0530

    [Ayush] Add: UC_2 Content to Main

---
+++ UC 2/AddressBook.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+        
+        System.out.println("Contact Added Successfully!");
+    }
+
+    public void displayContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("Address Book is Empty.");
+            return;
+        }
+
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+            System.out.println("---------------------------");
+        }
+    }
+}
